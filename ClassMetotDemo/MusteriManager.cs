6be873b5262fffe1ffa8607fbf306191da492bce
SERDAR YILMAZ <serdar.yilmaz202@ogr.atauni.edu.tr>
2021-01-31 19:03:32 +0300

    31.01.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+          
+            Console.WriteLine("Müşteri " + musteri.Name + " eklendi");
+        }
+        public void Remove(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Name + " silsindi");
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri " + musteri.Name + " güncellendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 123;
+            musteri1.Name = "Serdar";
+            musteri1.Surname = "Yılmaz";
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 124;
+            musteri2.Name = "Serkan";
+            musteri2.Surname = "Yılmaz";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Remove(musteri1);
+            musteriManager.Update(musteri2);
+        }
+        
+        
+    }
+}
