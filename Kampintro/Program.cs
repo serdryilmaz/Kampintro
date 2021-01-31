3a991aa0ec653be4549df4ce3dc14d423b128a45
SERDAR YILMAZ <serdar.yilmaz202@ogr.atauni.edu.tr>
2021-01-31 15:21:53 +0300

    31.01.2021

---
+++ Kampintro/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kampintro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "serdar";
+            int sayi = 32000;
+            bool giris = false;
+            if (giris == true)
+            {
+                Console.WriteLine("Sisteme Giriş Yapılmıştır.");
+
+            }
+            else
+            {
+                Console.WriteLine("Sisteme Giriş Yapılmamıştır.");
+
+            }
+        }
+    }
+}
